14ed3ef4eda39d7fbcf522b2b4ea3a2adee6c282
achtsyamaguchi <achtsan06@yahoo.co.jp>
2022-06-20 12:10:11 +0900

    LP作成

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+  $(window).scroll(function () {
+    $(".menu_item").each(function () {
+      var elemPos = $(this).offset().top; /* 要素の位置を取得 */
+      var scroll = $(window).scrollTop(); /* スクロール位置を取得 */
+      var windowHeight = $(window).height(); /* 画面幅を取得（画面の下側に入ったときに動作させるため） */
+      if (scroll > elemPos - windowHeight) {
+        /* 要素位置までスクロール出来たときに動作する */
+        $(this).addClass("effect_scroll");
+      }
+    });
+  });
+  jQuery(window).scroll();
+});
